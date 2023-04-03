4409c41f786289b253d30444bd95d0460c92d138
aaraujocl <69397063+aaraujocl@users.noreply.github.com>
2023-04-03 11:43:21 -0500

    Update app-yaganste

---
+++ Solucion 1/app-yaganaste/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './components/auth/auth.module';
+import { LoginService } from './components/service/login.service';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, AuthModule],
+  providers: [LoginService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
